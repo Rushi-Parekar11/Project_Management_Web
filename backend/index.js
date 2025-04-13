b34283747dc8415dbd694c1755debe4110ce4037
Rushi-Parekar11 <rushikeshparekar11@gmail.com>
2025-04-13 15:15:05 +0530

    deplaoy

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,7 @@ const cors = require("cors");
 
 const PORT = 8081;
 
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://dokjan.vercel.app'],
-  credentials: true
-}));
-
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/', router);
 
