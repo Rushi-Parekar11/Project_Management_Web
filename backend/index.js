61b0f41fd8e3003ba1ef8c6855d391dff99e960b
Rushi-Parekar11 <rushikeshparekar11@gmail.com>
2025-04-13 09:37:48 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,59 @@
-const express = require('express')
-const app = express()
-const port = 8081
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const app = express();
+const router = require('./Routes/Allroutes')
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const cors = require("cors");
+
+const PORT = process.env.PORT || 8000;
+
+//app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174'], // Allow multiple origins
+  credentials: true // If using cookies or authentication
+}));
+
+app.use(bodyParser.json());
+app.use('/', router);
+
+
+
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{
+  console.log("Connected Successfully.....");
+})
+.catch((err) => {
+  console.error("MongoDB Connection Error:", err);
+});
+
+
+
+app.get('/',(req,res)=>{
+  res.send("hii")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/ping',(req,res)=>{
+    res.send("PONGG");
 })
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("MongoDB Connected ✅");
+
+  // Start server only after DB is connected
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  });
+
+})
+.catch((err) => {
+  console.error("❌ MongoDB Connection Error:", err);
+});
+
+
+
